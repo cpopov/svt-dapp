f3a8d01a3a91c628b42181d86032702f87a7c1d6
Akhil BG <83879587+akhilbhadrangirija@users.noreply.github.com>
2024-08-08 13:50:15 +0530

    Fix typo

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -3,7 +3,7 @@ import PortfolioTable from '@/components/ProtfolioTable'
 export const metadata = {
   title: 'Portfolio'
 }
-export default function Protfolio() {
+export default function Portfolio() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between py-24 container">
       <PortfolioTable />
