2b8eb57896af31ba49d24ceb3d31caf86db490a8
Akhil BG <83879587+akhilbhadrangirija@users.noreply.github.com>
2024-08-08 13:46:51 +0530

    UI fixes

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import TradeButton from '@/components/TradeButton'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between py-24 container">
       <PlayerTable />
     </main>
   )
--- app/protfolio/page.js
+++ app/protfolio/page.js
@@ -5,7 +5,7 @@ export const metadata = {
 }
 export default function Protfolio() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between py-24 container">
       <PortfolioTable />
     </main>
   )
