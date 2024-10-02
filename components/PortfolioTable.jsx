aa3c0b2499994ffb1771a81ac6ff3bde57129e4b
Akhil BG <83879587+akhilbhadrangirija@users.noreply.github.com>
2024-10-02 22:09:44 +0530

    Update porfolio table

--- components/PortfolioTable.jsx
+++ components/PortfolioTable.jsx
@@ -98,7 +98,7 @@ const PlayerRow = ({ player, ...props }) => (
       <PlayerCell text={player.symbol} />
       <PlayerCell text={player.team} />
       <PlayerCell text={player.position} />
-      <PlayerCell text={`$ ${player.price}`} />
+      <PlayerCell text={player?.price ? `$ ${player?.price}` : ''} />
       <div className="md:min-w-[150px] sticky right-0 bg-white group-hover:bg-secondary pl-3 flex items-center">
         <TradeButton data={player} className="group-hover:hover" />
       </div>
