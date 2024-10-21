e4d93b70f59b9ddf16ec3ba4543c2d887748747f
Chris Popov <chrpopov@gmail.com>
2024-10-21 16:21:08 +0100

    update of the API endpoint due to changes server side

--- actions/index.js
+++ actions/index.js
@@ -2,6 +2,19 @@ import instance from './axios'
 
 const http = instance()
 
+/**
+ * Get the list of players
+ * @param sort one of 'name', 'team', 'price'
+ * @param dir one of 'asc' or 'desc'
+ * @returns {Promise<axios.AxiosResponse<any>>}
+ * TODO: filters and search. See docs for more info
+ */
 export const getPlayersList = ({ sort, dir }) =>
-  http.get(`/market?sort=${sort}&dir=${dir}`)
+  http.get(`/tokens/football?sort=${sort}&dir=${dir}`)
+
+/**
+ * Get the player details
+ * @param address token address
+ * @returns {Promise<axios.AxiosResponse<any>>}
+ */
 export const getUserPortfolio = address => http.get(`/portfolio/${address}`)
