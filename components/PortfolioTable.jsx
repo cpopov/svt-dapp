4080089168afe307af1451e005512550268a0a80
Akhil BG <83879587+akhilbhadrangirija@users.noreply.github.com>
2024-11-04 12:55:49 +0530

    Remove mytoken from portfolio

--- components/PortfolioTable.jsx
+++ components/PortfolioTable.jsx
@@ -45,7 +45,7 @@ const TableWrapper = ({ children, onSort, sortBy, sortDirection }) => (
   <Table>
     <TableHeader className="bg-[#DDEDE7]">
       <TableRow className="uppercase">
-        <TableHead className="hidden md:table-cell">My Tokens</TableHead>
+        {/* <TableHead className="hidden md:table-cell">My Tokens</TableHead> */}
         <TableHead className="cursor-pointer" onClick={() => onSort('name')}>
           <div className="flex items-center gap-1">
             Player {renderSortArrow('name', sortBy, sortDirection)}
@@ -83,16 +83,16 @@ const PlayerRow = ({ player }) => (
           />
         </div>
         <div>
-          <div className="flex items-end">
+          {/* <div className="flex items-end">
             <p className="text-[#47A847] font-bold">
               {Number(player?.amount)?.toFixed(2)}
               <span className="text-[#47A847] font-normal text-xs pl-1">
                 SVC
               </span>
             </p>
-          </div>
+          </div> */}
           <p className="text-text_accent font-bold">{player.name}</p>
-          <div className="flex pt-1 gap-2 items-center">
+          <div className="flex gap-2 items-center">
             <Badge
               className="rounded-full text-text_accent font-normal bg-[#EBEDF0] w-fit text-sm"
               variant="outline">
@@ -113,14 +113,14 @@ const PlayerRow = ({ player }) => (
         </div>
       </div>
     </TableCell>
-    <TableCell className="hidden md:table-cell">
+    {/* <TableCell className="hidden md:table-cell">
       <div className="flex items-end">
         <p className="text-[#47A847] font-bold">
           {Number(player?.amount)?.toFixed(2)}
           <span className="text-[#47A847] font-normal text-xs pl-1">SVC</span>
         </p>
       </div>
-    </TableCell>
+    </TableCell> */}
     <TableCell className="hidden md:table-cell">
       <div className="flex items-center h-full">
         <div className="relative h-14 w-14 mr-2 rounded-full overflow-clip group-hover:border-accent border">
